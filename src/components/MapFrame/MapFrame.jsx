13892b0fbec751186f340625976d1f9ac5aa4169
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2022-08-01 22:30:54 +0300

    [+] add random coordinates generator

--- src/components/MapFrame/MapFrame.jsx
+++ src/components/MapFrame/MapFrame.jsx
@@ -1,11 +1,28 @@
 import PropTypes from "prop-types"
 import { Map, Marker } from "pigeon-maps";
 import { osm } from 'pigeon-maps/providers';
+import { generateCoordinates } from "../../utils/utils";
+import { useLayoutEffect, useRef } from "react";
 
+const randomCoordinates = Array.from({length: 100}).map(() => generateCoordinates(50.450001, 30.523333, 10000));
+console.log(randomCoordinates);
 export default function MapFrame({defaultCenter, defaultZoom, updatePosition}) {
+  const mapRef = useRef(null);
+
+  useLayoutEffect(() => {
+    return () => {
+      const {center, zoom} = (mapRef.current.state);
+      updatePosition({center, zoom});
+    }
+  },[]);
+  const handleClick = ({anchor}) => {
+    console.log(anchor[0], anchor[1]);
+  }
+
   return (
-    <Map defaultCenter={defaultCenter} defaultZoom={defaultZoom} onBoundsChanged={updatePosition} provider={osm}>
-      <Marker width={50} anchor={[50.450001, 30.523333]} />
+    <Map onClick={console.log} ref={mapRef} mouseEvents defaultCenter={defaultCenter} defaultZoom={defaultZoom} provider={osm}>
+      <Marker width={50} anchor={[50.450001, 30.523333]} /> 
+      {randomCoordinates.map(spot => <Marker key={spot[0]} anchor={[spot[0], spot[1]]} onClick={handleClick} />)}
     </Map>
   )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,29 @@
-export function calculateDistance(latA, longA, latB, longB, meters = true) {
-  const degToRadian = deg => deg * Math.PI / 180;
+export function calculateDistance(latA, longA, latB, longB, km = true) {
+  const degToRadian = deg => (deg * Math.PI) / 180;
 
-  const earthRadius = meters ? 6371000 : 3958800;
+  const earthRadius = km ? 6371 : 3958.8;
   const distLat = degToRadian(latB - latA);
   const distLong = degToRadian(longB - longA);
-  const a = Math.sin(distLat / 2) ** 2 + Math.cos(degToRadian(latA)) * 
-    Math.cos(degToRadian(latB)) * Math.sin(distLong / 2) ** 2;
+  const a = Math.sin(distLat / 2) * Math.sin(distLat / 2) + Math.cos(degToRadian(latA)) * 
+    Math.cos(degToRadian(latB)) * Math.sin(distLong / 2) * Math.sin(distLong / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   
   return earthRadius * c;
+}
+
+export function generateRandomCoordinates(latLimit = 90, longLimit = 180) {
+  const latFactor = Math.random() > 0.5 ? 1 : -1;
+  const longFactor = Math.random() > 0.5 ? 1 : -1;
+  return [Math.random() * latLimit * latFactor, Math.random() * longLimit * longFactor];
+}
+
+export function generateCoordinates(lat, long, radius) {
+  const direction = () => Math.random() > 0.5 ? 1 : -1;
+  const shiftLimit = radius / 111000;
+  const latShift = (Math.random() * shiftLimit) * direction();
+  const longShift = (Math.random() * shiftLimit) * direction();
+
+  const result = [lat + latShift, long + longShift];
+
+  return [...result, calculateDistance(lat, long, result[0], result[1])];
 }
