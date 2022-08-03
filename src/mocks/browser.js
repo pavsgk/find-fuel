e31b6f670592cac192be3bbf71d4d4d109178286
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2022-08-03 22:05:31 +0300

    [+] add mock with gas stations

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -2,7 +2,7 @@ import { setupWorker, rest } from "msw";
 import stations from "./stations";
 
 const worker = setupWorker(
-  rest.get('/api/db', (req, res, ctx) => res(ctx.json(stations))),
+  rest.get('/api/stations', (req, res, ctx) => res(ctx.json(stations))),
 )
 
 worker.start({onUnhandledRequest: "bypass"});
