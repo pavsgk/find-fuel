93c619b63bdbdcb054dded81a766755ae98e1438
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2022-08-08 20:44:57 +0300

    [+] add debounce

--- src/store/reducers/stations.js
+++ src/store/reducers/stations.js
@@ -1,8 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { requestStations } from "../../utils/api";
+import { calculateDistance } from "../../utils/utils";
 
 const initialState = {
   db: [],
+  mark: [50.450001, 30.523333],
   filters: {},
   filtered: [],
   isReady: false,
@@ -22,7 +24,13 @@ const stationsSlice = createSlice({
   },
   extraReducers: {
     [getStations.fulfilled]: (state, {payload}) => {
-      state.db = payload;
+      const [lat, long] = state.mark;
+      console.log(lat, long)
+      state.db = payload.map(station => {
+        station['distance'] = calculateDistance(station['latitude'], station['longitude'], lat, long);
+        return station;
+      });
+
       state.isReady = true;
       state.isLoading = false;
     },
