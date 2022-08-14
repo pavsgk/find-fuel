61f87df607e017fb64a47f209f997e46b77617f9
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2022-08-15 00:32:34 +0300

    [*] fix msw and mocks for deploy

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,6 +1,6 @@
 import { setupWorker, rest } from "msw";
 import { urls } from "../utils/constants";
-import { stations } from "./tomtom_kyiv";
+import { stations } from "./tomtom";
 
 const worker = setupWorker(
   rest.get(urls.stations, (req, res, ctx) => res(ctx.delay(250), ctx.json(stations))),
